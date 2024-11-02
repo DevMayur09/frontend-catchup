03b53945cd56ccf7a52947321356aafc86de4742
mayur <mayur.thool@viklele.com>
2024-11-02 12:41:47 +0530

    persist the data in store and check if user is authenticated

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -1,8 +1,31 @@
-import React from "react";
+import React, { useEffect } from "react";
 import NavBar from "./NavBar";
-import { Outlet } from "react-router-dom";
+import { Outlet, useNavigate } from "react-router-dom";
+import axios from "axios";
+import { Base_url } from "../utils/constants";
+import { useDispatch, useSelector } from "react-redux";
+import { addUser } from "../utils/userSlice";
 
 const Body = () => {
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const userData = useSelector((store) => store.user);
+  const fetchUser = async () => {
+    try {
+      if (userData) return;
+      const $response = await axios.get(Base_url + "/profile/view");
+      dispatch(addUser($response.data.user));
+    } catch (error) {
+      if (error.status === 401) {
+        navigate("/login");
+      }
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    fetchUser();
+  }, []);
   return (
     <>
       <NavBar />

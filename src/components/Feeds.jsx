491d900f82dd3cdc3d9c84e5588828e160be9c74
mayur <mayur.thool@viklele.com>
2024-11-03 22:39:11 +0530

    feed component created

--- src/components/Feeds.jsx
+++ src/components/Feeds.jsx
@@ -1,7 +1,34 @@
-import React from "react";
+import axios from "axios";
+import React, { useEffect } from "react";
+import { Base_url } from "../utils/constants";
+import { useDispatch, useSelector } from "react-redux";
+import { addFeeds } from "../utils/feedSlice";
+import UserCard from "./UserCard";
 
 const Feeds = () => {
-  return <div>Feeds</div>;
+  const feeds = useSelector((state) => state.feeds);
+  const dispatch = useDispatch();
+
+  const fetchFeeds = async () => {
+    try {
+      if (feeds) return;
+      const $response = await axios.get(Base_url + "/user/feeds", { withCredentials: true });
+      dispatch(addFeeds($response.data));
+    } catch (error) {
+      console.log("Error : ", error.message);
+    }
+  };
+
+  useEffect(() => {
+    fetchFeeds();
+  }, []);
+  return (
+    feeds && (
+      <div>
+        <UserCard user={feeds.data[0]} />
+      </div>
+    )
+  );
 };
 
 export default Feeds;
--- src/utils/appStore.js
+++ src/utils/appStore.js
@@ -1,8 +1,9 @@
 import { configureStore } from "@reduxjs/toolkit";
 import userSlice from "./userSlice";
+import feedSlice from "./feedSlice";
 
 const appStore = configureStore({
-  reducer: { user: userSlice },
+  reducer: { user: userSlice, feeds: feedSlice },
 });
 
 export default appStore;
